681c17bc34b86759c5a6a7981bd7fd05d1a51a17
Dasarisravani16 <dasarisravani209@gmail.com>
2025-01-18 08:57:44 +0530

    Add files via upload

---
+++ sravani.py
@@ -0,0 +1,178 @@
+'''
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+''' 
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(i+1):
+        
+        print("*",end=" ")
+    print()
+    
+'''
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+'''
+
+n=int(input("n:"))
+for i in range(n):
+   for j in range(i,n):
+       print("*",end=" ")
+   print()
+   
+'''
+* * * * * 
+    * * * * 
+      * * * 
+        * * 
+          *
+ '''
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+    
+'''    
+          * 
+        * * 
+      * * * 
+    * * * * 
+  * * * * * 
+'''
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
+'''
+
+          * 
+        * * * 
+      * * * * * 
+    * * * * * * * 
+  * * * * * * * * * 
+'''
+
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
+'''
+
+ * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+'''
+
+n=int(input("n:"))
+for i in range( n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+'''    
+          * 
+        * * * 
+      * * * * * 
+    * * * * * * * 
+  * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+'''
+
+
+n=int(input("n:"))
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range( n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+    
+'''
+
+        * * * * * 
+      * * * * * 
+    * * * * * 
+  * * * * * 
+* * * * * 
+'''
+
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for j in range(n):
+        print("*",end=" ")
+    print()
+    
+'''
+
+*                 * 
+* *             * * 
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * *
+
+'''
+
+n=int(input("n:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range(2*(n-i-1)):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()    
+
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    for space in range(2*i):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()    
+
